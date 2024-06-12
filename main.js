368249db69728abb48f568b68bc6fd03cfd8d55e
Andres Briseno <abrisenocristerna@gmail.com>
2024-06-12 07:23:31 -0600

    Mi portafolio V1

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+  strings: [
+    "FrontEnd Developer",
+    "Backend Developer",
+    "Full-Stack Developer",
+    "Web Developer",
+    "Mobile App Developer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
